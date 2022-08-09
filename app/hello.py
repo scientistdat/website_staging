c54b9590f0be02ed206398f2e6cf2cc5ed4555e4
Scientist.Dat <65382714+scientistdat@users.noreply.github.com>
2022-08-09 14:40:13 -0400

    new files

--- app/hello.py
+++ app/hello.py
@@ -1,4 +1,22 @@
+from sre_constants import RANGE_UNI_IGNORE
 from flask import Flask, render_template
+import pandas as pd 
+import plotly
+import plotly.graph_objs as go
+
+import pandas as pd
+import numpy as np
+import json
+
+def create_plot():
+    df = pd.read_csv('/Users/pawar/Build/Website/Daily activity metrics.csv')
+    fig = go.Figure()
+    fig = fig.add_trace(go.Bar(x=df['Date'],y=df['Step count']))
+    fig = fig.update_layout(title_text="My Step Count",xaxis={'rangeslider':{'visible':True}})
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -12,8 +30,10 @@ def blog():
     return render_template('blog.html')
 
 @app.route('/projects/')
+
 def projects():
-    return render_template('projects.html')
+    bar = create_plot()
+    return render_template('projects.html',plot = bar)
 
 
 if __name__ == '__main__':
